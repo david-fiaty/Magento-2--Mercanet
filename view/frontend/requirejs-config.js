ddd716db0599bd0c37ff1ab75853b7620e0da7ae
david-fiaty-cko <david.fiaty@checkout.com>
2019-01-03 19:11:09 +0000

    Removed unused javascript code

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -7,23 +7,4 @@
  * License GNU/GPL V3 https://www.gnu.org/licenses/gpl-3.0.en.html
  */
 
-/*
-var config = {
-    map: {
-        '*': {
-            'Magento_Checkout/js/model/place-order': 'Cmsbox_Mercanet/js/model/place-order',
-            'Magento_Checkout/js/model/error-processor': 'Cmsbox_Mercanet/js/model/error-processor'
-        }
-    }
-};
-*/
-
-var config = {
-    config: {
-            mixins: {
-                'Magento_Ui/js/view/messages': {
-                    'Cmsbox_Mercanet/js/messages-mixin': true
-                }
-            }
-        }
-    };
+var config = {};

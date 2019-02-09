6ed377e374b90de8f4b9d805fadded8871b086a4
david-fiaty-cko <david.fiaty@checkout.com>
2019-02-09 18:39:58 +0000

    Updated registration.php

--- registration.php
+++ registration.php
@@ -8,10 +8,10 @@
  * License GNU/GPL V3 https://www.gnu.org/licenses/gpl-3.0.en.html
  */
 
-const MODULE_NAME = 'Cmsbox_Mercanet';
+use Cmsbox\Mercanet\Gateway\Config\Core;
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    MODULE_NAME, 
+    Core::moduleName(), 
     __DIR__
 );

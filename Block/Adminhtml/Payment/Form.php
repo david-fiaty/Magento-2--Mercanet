624ead35eb2b00884a26a11a209d3ea35e1e679e
david-fiaty-cko <david.fiaty@checkout.com>
2019-02-11 14:24:10 +0000

    Updated author tag

--- Block/Adminhtml/Payment/Form.php
+++ Block/Adminhtml/Payment/Form.php
@@ -6,7 +6,7 @@
  *
  * @category  Cmsbox
  * @package   Mercanet
- * @author    Cmsbox.fr <contact@cmsbox.fr> 
+ * @author    Cmsbox France <contact@cmsbox.fr> 
  * @copyright Cmsbox.fr all rights reserved.
  * @license   https://opensource.org/licenses/mit-license.html MIT License
  * @link      https://www.cmsbox.fr

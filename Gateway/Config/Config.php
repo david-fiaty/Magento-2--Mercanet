ccfefb1b2f2d7c350470d726fb0ffe9853a99080
david-fiaty-cko <david.fiaty@checkout.com>
2019-02-11 13:54:30 +0000

    Added PHP version to file headers

--- Gateway/Config/Config.php
+++ Gateway/Config/Config.php
@@ -5,6 +5,7 @@
  * Copyright (c) 2017 Cmsbox.fr (https://www.cmsbox.fr)
  * Author: David Fiaty | contact@cmsbox.fr
  *
+ * PHP version 7
  * License GNU/GPL V3 https://www.gnu.org/licenses/gpl-3.0.en.html
  */
  

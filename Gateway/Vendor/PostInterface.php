694f0d9fc352d0ffcc403b4582cb452dcc79b9e0
david-fiaty-cko <david.fiaty@checkout.com>
2019-03-14 18:35:35 +0000

    Fixed class path in vendor file

--- Gateway/Vendor/PostInterface.php
+++ Gateway/Vendor/PostInterface.php
@@ -241,7 +241,7 @@ class PostInterface
         if (strlen($street) > 35) {
             throw new \InvalidArgumentException("street is too long");
         }
-        $this->parameters['billingAddress.street'] = Normalizer::normalize($street);
+        $this->parameters['billingAddress.street'] = \Normalizer::normalize($street);
     }
 
     public function setBillingAddressStreetNumber($nr)
@@ -249,7 +249,7 @@ class PostInterface
         if (strlen($nr) > 10) {
             throw new \InvalidArgumentException("streetNumber is too long");
         }
-        $this->parameters['billingAddress.streetNumber'] = Normalizer::normalize($nr);
+        $this->parameters['billingAddress.streetNumber'] = \Normalizer::normalize($nr);
     }
 
     public function setBillingAddressZipCode($zipCode)
@@ -257,7 +257,7 @@ class PostInterface
         if (strlen($zipCode) > 10) {
             throw new \InvalidArgumentException("zipCode is too long");
         }
-        $this->parameters['billingAddress.zipCode'] = Normalizer::normalize($zipCode);
+        $this->parameters['billingAddress.zipCode'] = \Normalizer::normalize($zipCode);
     }
 
     public function setBillingAddressCity($city)
@@ -265,7 +265,7 @@ class PostInterface
         if (strlen($city) > 25) {
             throw new \InvalidArgumentException("city is too long");
         }
-        $this->parameters['billingAddress.city'] = Normalizer::normalize($city);
+        $this->parameters['billingAddress.city'] = \Normalizer::normalize($city);
     }
 
     public function setBillingContactPhone($phone)
@@ -278,12 +278,12 @@ class PostInterface
 
     public function setBillingContactFirstname($firstname)
     {
-        $this->parameters['billingContact.firstname'] = str_replace(array("'", '"'), '', Normalizer::normalize($firstname)); // replace quotes
+        $this->parameters['billingContact.firstname'] = str_replace(array("'", '"'), '', \Normalizer::normalize($firstname)); // replace quotes
     }
 
     public function setBillingContactLastname($lastname)
     {
-        $this->parameters['billingContact.lastname'] = str_replace(array("'", '"'), '', Normalizer::normalize($lastname)); // replace quotes
+        $this->parameters['billingContact.lastname'] = str_replace(array("'", '"'), '', \Normalizer::normalize($lastname)); // replace quotes
     }
     
     public function setCaptureDay($number)
